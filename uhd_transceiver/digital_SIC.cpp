0df0bd782550756448ca5eddb22844be496ebee5
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-08-03 22:33:48 -0400

    modify signal generation

--- uhd_transceiver/digital_SIC.cpp
+++ uhd_transceiver/digital_SIC.cpp
@@ -35,7 +35,7 @@ MatrixXf x2A(VectorXf& x, int k)
 	return A;
 }
 
-int dg_sync(VectorXf& preamble, VectorXf& rbuff)
+int dg_sync(VectorXf& preamble, VectorXf& rbuff)   // return delay in rbuff
 {
 	int cor_length = rbuff.size() - preamble.size() + 1;
 	VectorXf Cor(cor_length);					   // make the preamble
@@ -89,45 +89,61 @@ VectorXf dg_cancel(VectorXf& sbuff, VectorXf& rbuff, VectorXf& h, int estimator_
 
 }
 
-//void main()
-//{
-//	// many preparations
-//	int estimator_length;
-//	int pilot_length;
-//	int samp_rate;
-//
-//	
-//	VectorXf tx_pilot, rx_pilot;     // should have complete definition later
-//
-//	// estimate
-//	VectorXf h;
-//	h = estimate(tx_pilot, rx_pilot, estimator_length);
-//
-//	// obtain new sequence of TX and RX
-//	VectorXf tx_data, rx_data;
-//	VectorXf y_clean;
-//	y_clean = dg_cancel(tx_data, rx_data, h, estimator_length);
-//
-//	// write to file and some other work
-//
-//}
-
-
-
 void main()
 {
-	// only for test
-	VectorXf x(10),y(7);
-	x<<1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
-	y<<-3, 8, -1, 0, 1, 2, 3;
-	cout<<"x: \n"<<x.transpose()<<endl;
-	cout<<"y: \n"<<y.transpose()<<endl;
-	int k = 2;
-	VectorXf h = estimate(x, y, 2*k);
-	cout<<"h : \n"<<h<<endl;
-	//MatrixXf m1 = x2A(x, x.size()-2*k, k);
-	//cout<<"x to A: \n"<<m1<<endl;
+	// many preparations
+	int estimator_length;
+	int preamble_length;
+	int pilot_length;
+	int signal_length;						   // the length of TX which made signal_length + delay <= RX's length, may be redefine
+	int samp_rate;
+	int k = estimator_length/2
 
+	VectorXf x, y;							   // initial signal got from UHD: here haven't defined complex number
+	VectorXf preamble;					       // should have complete definition later
 
+	int delay = dg_sync(preamble, y);
+
+	// define tx&rx_pilot and estimate h
+	VectorXf tx_pilot = VectorXf::block(preamble_length - k - 1, 0, pilot_length + 2*k - 1, 0);
+	VectorXf rx_pilot = VectorXf::block(delay + preamble_length, 0, pilot_length, 0);
+	VectorXf h = estimate(tx_pilot, rx_pilot, estimator_length);
+
+	// obtain new sequence of TX and RX
+	VectorXf tx_data = VectorXf::block(preamble_length + pilot_length - k, 0, signal_length - pilot_length - preamble_length + k, 0);
+	VectorXf rx_data = VectorXf::block(delay + preamble_length + pilot_length, 0, signal_length - pilot_length - preamble_length - k + 1, 0);
+	VectorXf y_clean;
+	y_clean = dg_cancel(tx_data, rx_data, h, estimator_length);
+
+	// write to file and some other work
+	ofstream outfile;
+	string name[3] = {"tx_file","rx_file","y_clean_file"};
+	VectorXf * ptr[3] = {&x, &y, &y_clean};									 // may not work
+	for (int i = 0; i < 3; i++)
+	{
+		outfile.open(name[i].c_str(), ios::out | ios::binary);				 // for Matlab visualization
+		outfile.write((const char*)ptr[i], signal_length*sizeof(float));     // try I/Q channel by one first
+		outfile.close();
+	}
 
 }
+
+
+
+//void main()
+//{
+//	// only for test
+//	VectorXf x(10),y(7);
+//	x<<1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
+//	y<<-3, 8, -1, 0, 1, 2, 3;
+//	cout<<"x: \n"<<x.transpose()<<endl;
+//	cout<<"y: \n"<<y.transpose()<<endl;
+//	int k = 2;
+//	VectorXf h = estimate(x, y, 2*k);
+//	cout<<"h : \n"<<h<<endl;
+//	//MatrixXf m1 = x2A(x, x.size()-2*k, k);
+//	//cout<<"x to A: \n"<<m1<<endl;
+//
+//
+//
+//}
