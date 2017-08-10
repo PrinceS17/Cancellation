e1ca493c5712f100ddbeb736476bd8e62b6ce2c3
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-08-10 16:34:19 -0400

    Read the pilot and plot it

--- matlab_sic/test_trans_canc.m
+++ matlab_sic/test_trans_canc.m
@@ -1,39 +1,66 @@
 % test tranceiver_canceler
 close all
 clear all
+addpath(genpath('/home/flexicon/Documents/Cancellation/Cancellation'));
 build_path = {'/home/flexicon/Downloads/GNURadio/uhd/host/examples/transceiver_canceler/build';
     '/home/flexicon/Downloads/GNURadio/uhd/host/examples/transceiver_canceler_multi_tone/build'};
-filename = {'tx_file','rx_file','y_clean_file';
-    'tx_file_mt','rx_file_mt','y_clean_file_mt'};
+filename = {'tx_file','rx_file','y_clean_file','estimated_pilot','rx_pilot';
+    'tx_file_mt','rx_file_mt','y_clean_file_mt','estimated_pilot_mt','rx_pilot_mt'};
 
-id = 1;           % switch: 1 for transceiver_canceler; 2 for multi_tone
+id = 2;           % switch: 1 for transceiver_canceler; 2 for multi_tone
 
+[~,w] = size(filename);
+a = cell(1,w);
 addpath(genpath(build_path{id}));
-fid = fopen(filename{id,1});              % read x
-x = fread(fid,[1,inf],'float');
-fclose(fid);
 
-fid = fopen(filename{id,2});              % read y
-y = fread(fid,[1,inf],'float');
+for k = 1:w
+fid = fopen(filename{id,k});              % read x, y, y_clean and pilot
+a{k} = fread(fid,[1,inf],'float');
 fclose(fid);
+end
 
-fid = fopen(filename{id,3});              % read y_clean
-y_clean = fread(fid,[1,inf],'float');
-fclose(fid);
+x = a{1};
+y = a{2};
+y_clean = a{3};
+est_pilot = a{4};
+rx_pilot = a{5};
+
 % subplot(2,1,1);
 % plot(a1(1,:),':r'); hold on
 % plot(a2(1,:));
 % legend('y','y_{clean}');
 
-% plot
+% plot pilot
 rate = 2e6;   % should consistent with the C++ code!!!
 f = 100e3;
 L = length(y);
 t = (1:L)/rate;
-% x = 0.5*sin(2*pi*f*t);
-figure
-plot(t,x,':r',t,y,'b'); hold on;
-plot(t(1:length(y_clean)),y_clean,'g'); % xlim([0 8e-5]); ylim([-0.1 0.1]);
-% aksdjflkajf;jsaylim([-0.25 0.25]); xlim([0 1e-4]);
+if id == 1
+    preamble_length = 20;
+    pilot_length = 400;
+else
+    preamble_length = 20;
+    pilot_length = 600;
+end
 
+figure
+id_pilot = preamble_length + 1:preamble_length + pilot_length; 
+plot(t(id_pilot),x(id_pilot),':r',t(id_pilot),rx_pilot,'b',t(id_pilot),est_pilot,'--g');
+title('TX, RX and estimated pilot');
+xlabel('time /s'); ylabel('amplitude');
+legend('TX pilot','RX pilot','estimated pilot');
+
+
+% plot data in time domain
+
+% figure
+% plot(t,x,':r',t,y,'b'); hold on;
+% plot(t(1:length(y_clean)),y_clean,'g'); % xlim([0 8e-5]); ylim([-0.1 0.1]);
+% % ylim([-0.25 0.25]); xlim([0 1e-4]);
+% title('TX, RX and canceled data');
+% xlabel('time /s'); ylabel('amplitude');
+% legend('TX data','RX data','canceled data');
+% 
+% 
+% 
 plot_sic(x,y,y_clean,rate);
