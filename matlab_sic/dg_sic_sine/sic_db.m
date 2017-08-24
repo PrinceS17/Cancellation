20402cb64e0e54478eaa68926dc74dcef388d605
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-08-24 13:55:24 -0400

    Add range to search for true fc.

--- matlab_sic/dg_sic_sine/sic_db.m
+++ matlab_sic/dg_sic_sine/sic_db.m
@@ -1,17 +1,28 @@
-function result = sic_db(signal, rate, fc, bw)
+function result = sic_db(signal, rate, fc, bw, rg)
 % calculate cancellation result: signal is matrix of RX and y_clean, rate is % sampling rate, 
 % fc is center frequency, bw is bandwidth which can be calculated theoretically, 
-% result is column vector of cancellation results
+% result is column vector of cancellation results; rg is the search range
+% for true fc (may be a shift off given fc)
 
 P_db = zeros(2,1);
+[~, L] = size(signal);
+fl_id = round(((fc - rg/2)/rate + 0.5)*L);
+fr_id = round(((fc + rg/2)/rate + 0.5)*L);
 for i = 1:2
     x = signal(i,:);
-    fx = abs(fft(x))/length(x)*2;
+    fx = abs(fft(x))/L*2;
     Px = 10*log10(fx.^2/100*1000); 
     Px = fftshift(Px);
     
-    fl_id = ((fc - bw/2)/rate + 0.5)*length(x);
-    fr_id = ((fc + bw/2)/rate + 0.5)*length(x);
+     % search from [-bw,0] to [0,bw]
+    
+    if i == 1
+        [~,peak_id] = max(Px(fl_id:fr_id));             % for searching max
+        ofst = peak_id - round((rg/2)*L/rate);
+        fc = fc + ofst*rate/L;
+        fl_id = round(((fc - bw/2)/rate + 0.5)*L);      % for calculate cancellation
+        fr_id = round(((fc + bw/2)/rate + 0.5)*L);
+    end
     
     P_db(i) = mean(Px(fl_id:fr_id));
 end
