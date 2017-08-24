fcc86fb42d74136012d24c5b709c43de161c6c58
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-08-23 23:55:39 -0400

    Change x2A for nonlinearity.

--- uhd_transceiver/nonlinear.cpp
+++ uhd_transceiver/nonlinear.cpp
@@ -34,3 +34,25 @@ MatrixXf x2A(VectorXf & x, int k)	        // default dim is 1
 {
 	return x2A(x, k, 1);
 }
+
+/*
+ // record linear cancellation for back up
+MatrixXf x2A(VectorXf& x, int l, int k)		// Note A(0,0) is x(0), so the 1st y is y(k)
+{
+	int n = l - 1;							// l is the length of [x0, x1, ..., xn]
+	int st = k;								// normally x.size() should not be less than n + 2k
+	MatrixXf A(n + 1, 2*k);					// should be check a third time!
+	for(int i = 0; i < n + 1; i++)
+	for(int j = 0; j < 2*k; j++)
+	{
+		A(i,j) = x(st - k + j + i);
+ 	}
+	return A;
+}
+
+MatrixXf x2A(VectorXf& x, int k)
+{
+	int lx = x.size();	
+	MatrixXf A = x2A(x, lx - 2*k + 1, k);			   // every points are used
+	return A;
+}*/
--- uhd_transceiver/transceiver_canceler_rt_sync.cpp
+++ uhd_transceiver/transceiver_canceler_rt_sync.cpp
@@ -45,24 +45,30 @@ void sig_int_handler(int){stop_signal_called = true;}
 // 1, matrix A and A_inv: now calculated by function, in fact should store into a global object
 // 2, MatrixXf: now use float real type, MatrixXcf probably needed in the future 
 
-MatrixXf x2A(VectorXf& x, int l, int k)		// Note A(0,0) is x(0), so the 1st y is y(k)
+// x2A: contains nonlinearity, dim = 1 means linear by default
+MatrixXf x2A(VectorXf & x, int l, int k, int dim)     //A(0,0) is x(0), 1st y is y(k)
 {
-	int n = l - 1;							// l is the length of [x0, x1, ..., xn]
-	int st = k;								// normally x.size() should not be less than n + 2k
-	MatrixXf A(n + 1, 2*k);					// should be check a third time!
-	for(int i = 0; i < n + 1; i++)
-	for(int j = 0; j < 2*k; j++)
-	{
-		A(i,j) = x(st - k + j + i);
- 	}
+	int n = l - 1;				// l: length of [x0,...,xn]
+	int st = k;					// start of y, x.size() should >= n + 2k, better =
+	//int dim = 4;				// dimension of nonlinearity
+	
+	MatrixXf A(n + 1, 2*k*dim);
+	for(int i = 0; i < n + 1; i ++)
+	for(int j = 0; j < 2*k; j ++)
+	for(int p = 0; p < dim; p ++)
+		A(i, j*dim + p) = pow(x(st - k + j + i),p + 1);		// x^(p + 1)
 	return A;
 }
 
-MatrixXf x2A(VectorXf& x, int k)
+MatrixXf x2A(VectorXf & x, int k, int dim)
 {
-	int lx = x.size();	
-	MatrixXf A = x2A(x, lx - 2*k + 1, k);			   // every points are used
-	return A;
+	int lx = x.size();
+	return x2A(x, lx - 2*k + 1, k, dim);
+}
+
+MatrixXf x2A(VectorXf & x, int k)	        // default dim is 1
+{
+	return x2A(x, k, 1);
 }
 
 class greater1
