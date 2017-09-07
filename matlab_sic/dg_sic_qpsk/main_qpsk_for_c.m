e73dd1e2fb2b64feda3687dbdc3642a086ba56e4
PrinceS17 <sjh14@mails.tsinghua.edu.cn>
2017-09-06 21:01:44 -0400

    Modify part for data file.

--- matlab_sic/dg_sic_qpsk/main_qpsk_for_c.m
+++ matlab_sic/dg_sic_qpsk/main_qpsk_for_c.m
@@ -1,9 +1,9 @@
 % main_qpsk reading TX and RX from file of qpsk_transceiver1.cpp
 
 close all
-clear all
-path = '/home/flexicon/Downloads/GNURadio/uhd/host/examples/qpsk_transceiver1/build/';
-name = {'tx_out','qpsk_2M'};
+clear
+path = '';
+name = {'tx_qpsk','rx_qpsk'};
 
 %% read TX, RX from file
 a = cell(1,2);
@@ -14,8 +14,8 @@ for i = 1:2
 end
    
 %  1, it is real number and do the cancellation only for real
-tx = a{1}(1,:);     
-rx = a{2}(1,:);
+% tx = a{1}(1,:);     
+% rx = a{2}(1,:);
 
 % 2, use the complex number
 tx = a{1}(1,:) + 1i*a{1}(2,:);     
@@ -28,8 +28,8 @@ plot(1:length(rx),real(rx),'b');
 
 %% detect the correct buffer manually
 buff_length = 2e4;        % total length of the window, may not all the signal
-st1 = 797162;
-st2 = 797162;
+st1 = 510992;             % st1, st2 only for default file, change with signals
+st2 = 517442;
 tx_buff = tx(st1:st1 + buff_length - 1);
 rx_buff = rx(st2:st2 + buff_length - 1);
 
